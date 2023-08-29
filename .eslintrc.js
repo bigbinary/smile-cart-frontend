bf59cf8ebd7d5903a4bccc79eb396012252a669e
sruthy-j-mallya <sruthyjmallya2018@gmail.com>
2023-08-29 18:00:41 +0530

    Disabled eslint rules at project level

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,6 +14,13 @@ module.exports = mergeDeepLeft(
         },
       ],
       "@bigbinary/neeto/no-dumb-components-with-use-translation": "off",
+      "@bigbinary/neeto/ensure-zustand-stores-are-type-annotated": "off",
+      "@bigbinary/neeto/zustand-use-with-immutable-actions-wrapper-inside-create":
+        "off",
+      "@bigbinary/neeto/no-axios-import-outside-apis": "off",
+      "@bigbinary/neeto/use-functions-and-hooks-from-neeto-commons-frontend":
+        "off",
+      "@bigbinary/neeto/use-common-constants": "off",
     },
   },
   defaultConfig
