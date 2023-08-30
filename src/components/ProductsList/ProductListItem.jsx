364c586b9a7a3b7426b0572e4587cae8c82661ca
Sruthy J Mallya <53861843+sruthy-j-mallya@users.noreply.github.com>
2023-08-30 13:41:37 +0530

    Added buildUrl method to utils (#26)

--- src/components/ProductsList/ProductListItem.jsx
+++ src/components/ProductsList/ProductListItem.jsx
@@ -1,8 +1,9 @@
-/* eslint-disable @bigbinary/neeto/use-common-routes */
 import React from "react";
 
 import { Typography } from "neetoui";
 import { Link } from "react-router-dom";
+import routes from "routes";
+import { buildUrl } from "utils/url";
 
 import AddToCart from "../AddToCart";
 
@@ -15,7 +16,7 @@ const ProductListItem = ({
 }) => (
   <Link
     className="neeto-ui-border-black neeto-ui-rounded-xl flex w-48 flex-col items-center justify-between border p-4"
-    to={`products/${slug}`}
+    to={buildUrl(routes.product.show, { slug })}
   >
     <img alt={name} className="h-40 w-40" src={imageUrl} />
     <Typography className="text-center" weight="semibold">
