0e05a7351eddd543ec23b82986c18657e0f41fb2
Sruthy J Mallya <53861843+sruthy-j-mallya@users.noreply.github.com>
2023-09-05 09:48:35 +0530

    Added persist middleware (#29)

--- src/hooks/reactQuery/useProductsApi.js
+++ src/hooks/reactQuery/useProductsApi.js
@@ -1,14 +1,29 @@
 import { DEFAULT_STALE_TIME, QUERY_KEYS } from "constants/query";
 
 import productsApi from "apis/products";
+import { Toastr } from "neetoui";
 import { prop } from "ramda";
+import { useTranslation } from "react-i18next";
 import { useQuery, useQueries } from "react-query";
+import useCartItemsStore from "stores/useCartItemsStore";
 
 export const useFetchCartProducts = slugs => {
+  const { cartItems, setSelectedQuantity } = useCartItemsStore.pick();
+  const { t } = useTranslation();
+
   const responses = useQueries(
     slugs.map(slug => ({
       queryKey: [QUERY_KEYS.PRODUCTS, slug],
       queryFn: () => productsApi.show(slug),
+      onSuccess: ({ availableQuantity, name }) => {
+        if (availableQuantity >= cartItems[slug]) return;
+        setSelectedQuantity(slug, availableQuantity);
+        if (availableQuantity === 0) {
+          Toastr.error(t("product.error.removedFromCart", { name }), {
+            autoClose: 2000,
+          });
+        }
+      },
       staleTime: DEFAULT_STALE_TIME,
     }))
   );
--- src/stores/useCartItemsStore.js
+++ src/stores/useCartItemsStore.js
@@ -1,19 +1,25 @@
 import { isNotEmpty } from "neetocommons/pure";
 import { assoc, dissoc, evolve } from "ramda";
 import { create } from "zustand";
+import { persist } from "zustand/middleware";
 
-const useCartItemsStore = create(set => ({
-  cartItems: {},
-  setSelectedQuantity: (slug, quantity) =>
-    set(({ cartItems }) => {
-      if (quantity <= 0 && isNotEmpty(quantity)) {
-        return { cartItems: dissoc(slug, cartItems) };
-      }
+const useCartItemsStore = create(
+  persist(
+    set => ({
+      cartItems: {},
+      setSelectedQuantity: (slug, quantity) =>
+        set(({ cartItems }) => {
+          if (quantity <= 0 && isNotEmpty(quantity)) {
+            return { cartItems: dissoc(slug, cartItems) };
+          }
 
-      return { cartItems: assoc(slug, String(quantity), cartItems) };
+          return { cartItems: assoc(slug, String(quantity), cartItems) };
+        }),
+      removeCartItem: slug => set(evolve({ cartItems: dissoc(slug) })),
+      clearCart: () => set({ cartItems: {} }),
     }),
-  removeCartItem: slug => set(evolve({ cartItems: dissoc(slug) })),
-  clearCart: () => set({ cartItems: {} }),
-}));
+    { name: "cart-items-store" }
+  )
+);
 
 export default useCartItemsStore;
