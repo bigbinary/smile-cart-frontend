13048b0ff2dfb28f88607c23770ffba45e386593
Anmol Kumar <67572237+anmol-kumar0815@users.noreply.github.com>
2023-09-25 13:49:22 +0530

    Fixed routes object path (#32)

--- src/components/ProductsList/ProductListItem.jsx
+++ src/components/ProductsList/ProductListItem.jsx
@@ -16,7 +16,7 @@ const ProductListItem = ({
 }) => (
   <Link
     className="neeto-ui-border-black neeto-ui-rounded-xl flex w-48 flex-col items-center justify-between border p-4"
-    to={buildUrl(routes.product.show, { slug })}
+    to={buildUrl(routes.products.show, { slug })}
   >
     <img alt={name} className="h-40 w-40" src={imageUrl} />
     <Typography className="text-center" weight="semibold">
