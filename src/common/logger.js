e8d6a8b057c8329706d6146ccde83d7cd5d836db
sruthy-j-mallya <sruthyjmallya2018@gmail.com>
2023-11-23 19:18:53 +0530

    Removed rails env check in logger.js

--- src/common/logger.js
+++ src/common/logger.js
@@ -2,7 +2,4 @@ export const initializeLogger = () => {
   /* eslint react-hooks/rules-of-hooks: "off" */
   const Logger = require("js-logger");
   Logger.useDefaults();
-  if (process.env.RAILS_ENV === "production") {
-    Logger.setLevel(Logger.OFF);
-  }
 };
