bd950c153947586e73392e5a7ce94f0c756ea442
jaimysimon <jaimy.simon@bigbinary.com>
2023-12-08 19:08:41 +0530

    Added product page

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,4 @@
-import "./App.css";
-// eslint-disable-next-line import/extensions
-import logo from "./logo.svg";
+import Product from "./components/Product";
 
-const App = () => (
-  <div className="App">
-    <header className="App-header">
-      <img alt="logo" className="App-logo" src={logo} />
-      <p>
-        Edit <code>src/App.js</code> and save to reload.
-      </p>
-      <a
-        className="App-link"
-        href="https://reactjs.org"
-        rel="noopener noreferrer"
-        target="_blank"
-      >
-        Learn React
-      </a>
-    </header>
-  </div>
-);
-
+const App = () => <Product />;
 export default App;
