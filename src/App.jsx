d07c9df10f95280dff3a534d5b2ad0f5ad7095c9
Amaljith K <amaljith.k@bigbinary.com>
2023-12-11 11:35:14 +0530

    Lint fixes

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import "./App.css";
+// eslint-disable-next-line import/extensions
+import logo from "./logo.svg";
+
+const App = () => (
+  <div className="App">
+    <header className="App-header">
+      <img alt="logo" className="App-logo" src={logo} />
+      <p>
+        Edit <code>src/App.js</code> and save to reload.
+      </p>
+      <a
+        className="App-link"
+        href="https://reactjs.org"
+        rel="noopener noreferrer"
+        target="_blank"
+      >
+        Learn React
+      </a>
+    </header>
+  </div>
+);
+
+export default App;
