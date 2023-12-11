afc3d9c29e031fbe88380b8b425367bae269975f
Jaimy Simon <jaimy.simon@bigbinary.com>
2023-12-11 17:51:03 +0530

    Utilized updater function to set state values (#69)

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -7,16 +7,13 @@ import { Button } from "neetoui";
 const Carousel = ({ imageUrls, title }) => {
   const [currentIndex, setCurrentIndex] = useState(0);
 
-  const handleNext = () => {
-    const nextIndex = (currentIndex + 1) % imageUrls.length;
-    setCurrentIndex(nextIndex);
-  };
+  const handleNext = () =>
+    setCurrentIndex(prevIndex => (prevIndex + 1) % imageUrls.length);
 
-  const handlePrevious = () => {
-    const previousIndex =
-      (currentIndex - 1 + imageUrls.length) % imageUrls.length;
-    setCurrentIndex(previousIndex);
-  };
+  const handlePrevious = () =>
+    setCurrentIndex(
+      prevIndex => (prevIndex - 1 + imageUrls.length) % imageUrls.length
+    );
 
   return (
     <div className="flex flex-col items-center">
