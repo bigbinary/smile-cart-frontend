62fb3bafe822053bd5c0e3e697dcd718f1e2f0e6
Jaimy Simon <jaimy.simon@bigbinary.com>
2023-12-11 17:26:14 +0530

    Added Carousel component (#68)

---
+++ src/components/constants.js
@@ -0,0 +1,6 @@
+export const IMAGE_URLS = [
+  "https://i.dummyjson.com/data/products/9/thumbnail.jpg",
+  "https://i.dummyjson.com/data/products/9/1.jpg",
+  "https://i.dummyjson.com/data/products/9/2.png",
+  "https://i.dummyjson.com/data/products/9/3.png",
+];
