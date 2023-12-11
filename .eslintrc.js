0998715af8b9f7510944cb0589d84f7a8d600e2d
Amaljith K <amaljith.k@bigbinary.com>
2023-12-11 11:35:01 +0530

    Additional configs

---
+++ .eslintrc.js
@@ -0,0 +1,29 @@
+const defaultConfig = require("./config/eslint/index.js");
+const { mergeDeepLeft } = require("ramda");
+
+module.exports = mergeDeepLeft(
+  {
+    rules: {
+      "@bigbinary/neeto/webpack-aliases-and-jsconfig-paths-should-be-in-sync":
+        "off",
+      "@bigbinary/neeto/no-missing-localization": [
+        "error",
+        {
+          translationsDir: "./src/translations",
+          languages: ["en"],
+        },
+      ],
+      "@bigbinary/neeto/no-dumb-components-with-use-translation": "off",
+      "@bigbinary/neeto/ensure-zustand-stores-are-type-annotated": "off",
+      "@bigbinary/neeto/zustand-use-with-immutable-actions-wrapper-inside-create":
+        "off",
+      "@bigbinary/neeto/no-axios-import-outside-apis": "off",
+      "@bigbinary/neeto/use-functions-and-hooks-from-neeto-commons-frontend":
+        "off",
+      "@bigbinary/neeto/use-common-constants": "off",
+      "react/react-in-jsx-scope": "off",
+      "@bigbinary/neeto/hard-coded-strings-should-be-localized": "off",
+    },
+  },
+  defaultConfig
+);
