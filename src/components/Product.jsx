7463f40fedbd164fc8fce5ba54f6a259a524478b
Jaimy Simon <jaimy.simon@bigbinary.com>
2023-12-12 11:39:30 +0530

    Created product API connector file (#72)

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 
-import axios from "axios";
+import productsApi from "apis/products";
 import { Typography, Spinner } from "neetoui";
 import { append, isNotNil } from "ramda";
 
@@ -12,9 +12,7 @@ const Product = () => {
 
   const fetchProduct = async () => {
     try {
-      const response = await axios.get(
-        "https://smile-cart-backend-staging.neetodeployapp.com/products/infinix-inbook-2"
-      );
+      const response = await productsApi.show();
       setProduct(response.data);
     } catch (error) {
       console.log("An error occurred:", error);
