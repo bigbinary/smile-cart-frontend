252a07c0b1e704741197f9f4087ee18e87cf0779
Jaimy Simon <jaimy.simon@bigbinary.com>
2023-12-12 12:55:16 +0530

    Implemented Axios interceptors to set http headers and to transform response (#75)

--- src/apis/products.js
+++ src/apis/products.js
@@ -1,9 +1,6 @@
 import axios from "axios";
 
-const show = () =>
-  axios.get(
-    "https://smile-cart-backend-staging.neetodeployapp.com/products/infinix-inbook-2"
-  );
+const show = () => axios.get("products/infinix-inbook-2");
 
 const productsApi = { show };
 export default productsApi;
