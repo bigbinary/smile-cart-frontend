1b3d8facff5f8ef8d5c1c743ae2e5d207528693f
jaimysimon <jaimy.simon@bigbinary.com>
2023-12-13 18:40:48 +0530

    Added add to cart feature on the product details page

--- src/components/commons/Header.jsx
+++ src/components/commons/Header.jsx
@@ -1,15 +1,16 @@
+import { useContext } from "react";
+
 import { LeftArrow } from "neetoicons";
 import { Typography } from "neetoui";
 import { AiOutlineShoppingCart } from "react-icons/ai";
 import { useHistory } from "react-router-dom";
+import CartItemsContext from "src/contexts/CartItemsContext";
 
-const Header = ({
-  title,
-  shouldShowBackButton = true,
-  actionBlock,
-  cartItemsCount,
-}) => {
+const Header = ({ title, shouldShowBackButton = true, actionBlock }) => {
   const history = useHistory();
+
+  const [cartItems] = useContext(CartItemsContext);
+  const cartItemsCount = cartItems.length;
 
   return (
     <div className="m-2">
---
+++ src/contexts/CartItemsContext.js
@@ -0,0 +1,5 @@
+import { createContext } from "react";
+
+const CartItemsContext = createContext();
+
+export default CartItemsContext;
