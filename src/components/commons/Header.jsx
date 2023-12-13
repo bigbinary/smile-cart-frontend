fccc42697127856014f611851c2de1ae4c6891ea
Jaimy Simon <jaimy.simon@bigbinary.com>
2023-12-13 19:33:48 +0530

    Replaced CartItemsContext with useCartItemsStore (#85)

--- src/components/commons/Header.jsx
+++ src/components/commons/Header.jsx
@@ -1,16 +1,13 @@
-import { useContext } from "react";
-
 import { LeftArrow } from "neetoicons";
 import { Typography } from "neetoui";
 import { AiOutlineShoppingCart } from "react-icons/ai";
 import { useHistory } from "react-router-dom";
-import CartItemsContext from "src/contexts/CartItemsContext";
+import useCartItemsStore from "stores/useCartItemsStore";
 
 const Header = ({ title, shouldShowBackButton = true, actionBlock }) => {
   const history = useHistory();
 
-  const [cartItems] = useContext(CartItemsContext);
-  const cartItemsCount = cartItems.length;
+  const cartItemsCount = useCartItemsStore(store => store.cartItems.length);
 
   return (
     <div className="m-2">
