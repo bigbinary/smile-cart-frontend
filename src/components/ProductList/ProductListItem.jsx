6f65218e60e52d92929596dda74398203dcfc656
Jaimy Simon <jaimy.simon@bigbinary.com>
2023-12-13 12:36:00 +0530

    Standardized frontend routes (#78)

--- src/components/ProductList/ProductListItem.jsx
+++ src/components/ProductList/ProductListItem.jsx
@@ -1,10 +1,12 @@
 import { Typography } from "neetoui";
 import { Link } from "react-router-dom";
+import routes from "routes";
+import { buildUrl } from "utils/url";
 
 const ProductListItem = ({ imageUrl, name, offerPrice, slug }) => (
   <Link
     className="neeto-ui-border-black neeto-ui-rounded-xl flex w-48 flex-col items-center justify-between border p-4"
-    to={`products/${slug}`}
+    to={buildUrl(routes.products.show, { slug })}
   >
     <img alt={name} className="h-40 w-40" src={imageUrl} />
     <Typography className="text-center" weight="semibold">
