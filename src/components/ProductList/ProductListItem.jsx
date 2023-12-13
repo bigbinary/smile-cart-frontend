7fd74e6be588ab3c6e1a832acf896e4fd2b4dc53
jaimysimon <jaimy.simon@bigbinary.com>
2023-12-13 16:08:00 +0530

    Added feature to add products to cart

--- src/components/ProductList/ProductListItem.jsx
+++ src/components/ProductList/ProductListItem.jsx
@@ -1,9 +1,17 @@
+import AddToCart from "components/AddToCart";
 import { Typography } from "neetoui";
 import { Link } from "react-router-dom";
 import routes from "routes";
 import { buildUrl } from "utils/url";
 
-const ProductListItem = ({ imageUrl, name, offerPrice, slug }) => (
+const ProductListItem = ({
+  imageUrl,
+  name,
+  offerPrice,
+  slug,
+  isInCart,
+  toggleIsInCart,
+}) => (
   <Link
     className="neeto-ui-border-black neeto-ui-rounded-xl flex w-48 flex-col items-center justify-between border p-4"
     to={buildUrl(routes.products.show, { slug })}
@@ -13,6 +21,7 @@ const ProductListItem = ({ imageUrl, name, offerPrice, slug }) => (
       {name}
     </Typography>
     <Typography>${offerPrice}</Typography>
+    <AddToCart {...{ isInCart, toggleIsInCart }} />
   </Link>
 );
 
--- src/components/commons/Header.jsx
+++ src/components/commons/Header.jsx
@@ -1,8 +1,14 @@
 import { LeftArrow } from "neetoicons";
 import { Typography } from "neetoui";
+import { AiOutlineShoppingCart } from "react-icons/ai";
 import { useHistory } from "react-router-dom";
 
-const Header = ({ title, shouldShowBackButton = true, actionBlock }) => {
+const Header = ({
+  title,
+  shouldShowBackButton = true,
+  actionBlock,
+  cartItemsCount,
+}) => {
   const history = useHistory();
 
   return (
@@ -19,7 +25,17 @@ const Header = ({ title, shouldShowBackButton = true, actionBlock }) => {
             {title}
           </Typography>
         </div>
-        <div className="flex items-end space-x-4">{actionBlock}</div>
+        <div className="flex items-end space-x-4">
+          {actionBlock}
+          <div className="flex flex-col">
+            {cartItemsCount > 0 && (
+              <span className="neeto-ui-border-black neeto-ui-rounded-full min-w-fit flex h-5 w-5 items-center self-end border p-1">
+                {cartItemsCount}
+              </span>
+            )}
+            <AiOutlineShoppingCart size="2rem" />
+          </div>
+        </div>
       </div>
       <hr className="neeto-ui-bg-black h-1" />
     </div>
