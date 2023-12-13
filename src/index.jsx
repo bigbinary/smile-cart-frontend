50d9544ffebd8c0a0999019da07c8a5063ea575a
jaimysimon <jaimy.simon@bigbinary.com>
2023-12-13 19:44:52 +0530

    Initialized i18n

--- src/index.jsx
+++ src/index.jsx
@@ -5,6 +5,7 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 
 import App from "./App";
+import "./common/i18n";
 import "./index.css";
 import reportWebVitals from "./reportWebVitals";
 
