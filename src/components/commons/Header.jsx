61cb8ad99b94f82891a0c2bb1e9a8504ec139811
Jaimy Simon <jaimy.simon@bigbinary.com>
2023-12-13 13:45:36 +0530

    Implemented search functionality to product listing page (#79)

--- src/components/commons/Header.jsx
+++ src/components/commons/Header.jsx
@@ -2,21 +2,24 @@ import { LeftArrow } from "neetoicons";
 import { Typography } from "neetoui";
 import { useHistory } from "react-router-dom";
 
-const Header = ({ title, shouldShowBackButton = true }) => {
+const Header = ({ title, shouldShowBackButton = true, actionBlock }) => {
   const history = useHistory();
 
   return (
     <div className="m-2">
-      <div className="flex items-center">
-        {shouldShowBackButton && (
-          <LeftArrow
-            className="hover:neeto-ui-bg-gray-400 neeto-ui-rounded-full mr-6"
-            onClick={history.goBack}
-          />
-        )}
-        <Typography style="h1" weight="semibold">
-          {title}
-        </Typography>
+      <div className="mx-6 mb-2 mt-6 flex items-end justify-between">
+        <div className="flex items-center">
+          {shouldShowBackButton && (
+            <LeftArrow
+              className="hover:neeto-ui-bg-gray-400 neeto-ui-rounded-full mr-6"
+              onClick={history.goBack}
+            />
+          )}
+          <Typography style="h1" weight="semibold">
+            {title}
+          </Typography>
+        </div>
+        <div className="flex items-end space-x-4">{actionBlock}</div>
       </div>
       <hr className="neeto-ui-bg-black h-1" />
     </div>
