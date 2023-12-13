1fccd669fb5a94c9ba8c1246db34f9394a23c501
Jaimy Simon <jaimy.simon@bigbinary.com>
2023-12-13 18:49:06 +0530

    Added add to cart feature on the product details page (#84)

--- src/components/Product/index.jsx
+++ src/components/Product/index.jsx
@@ -1,7 +1,12 @@
 import { useState, useEffect } from "react";
 
 import productsApi from "apis/products";
-import { Header, PageLoader, PageNotFound } from "components/commons";
+import {
+  Header,
+  PageLoader,
+  PageNotFound,
+  AddToCart,
+} from "components/commons";
 import { Typography } from "neetoui";
 import { append, isNotNil } from "ramda";
 import { useParams } from "react-router-dom";
@@ -64,6 +69,7 @@ const Product = () => {
           <Typography className="font-semibold text-green-600">
             {discountPercentage}% off
           </Typography>
+          <AddToCart {...{ slug }} />
         </div>
       </div>
     </>
--- src/components/commons/index.js
+++ src/components/commons/index.js
@@ -1,5 +1,6 @@
+import AddToCart from "./AddToCart";
 import Header from "./Header";
 import PageLoader from "./PageLoader";
 import PageNotFound from "./PageNotFound";
 
-export { Header, PageNotFound, PageLoader };
+export { Header, PageNotFound, PageLoader, AddToCart };
