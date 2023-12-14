2d7fbe5e7859b05dc3c82eebb136ea1f29ab5d39
jaimysimon <jaimy.simon@bigbinary.com>
2023-12-14 15:02:46 +0530

    Added ability to set quantity of cart items

--- src/components/Product/index.jsx
+++ src/components/Product/index.jsx
@@ -33,7 +33,15 @@ const Product = () => {
     fetchProduct();
   }, []);
 
-  const { name, description, mrp, offerPrice, imageUrls, imageUrl } = product;
+  const {
+    name,
+    description,
+    mrp,
+    offerPrice,
+    imageUrls,
+    imageUrl,
+    availableQuantity,
+  } = product;
 
   const totalDiscounts = mrp - offerPrice;
   const discountPercentage = ((totalDiscounts / mrp) * 100).toFixed(1);
@@ -66,7 +74,7 @@ const Product = () => {
           <Typography className="font-semibold text-green-600">
             {discountPercentage}% off
           </Typography>
-          <AddToCart {...{ slug }} />
+          <AddToCart {...{ availableQuantity, slug }} />
         </div>
       </div>
     </>
--- src/components/commons/index.js
+++ src/components/commons/index.js
@@ -1,5 +1,6 @@
 import Header from "./Header";
 import PageLoader from "./PageLoader";
 import PageNotFound from "./PageNotFound";
+import TooltipWrapper from "./TooltipWrapper";
 
-export { Header, PageNotFound, PageLoader };
+export { Header, PageNotFound, PageLoader, TooltipWrapper };
