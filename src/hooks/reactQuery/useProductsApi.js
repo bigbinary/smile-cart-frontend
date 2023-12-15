fb9a655f2a814bea40ff6e3e7b095d6cd48bc4df
jaimysimon <jaimy.simon@bigbinary.com>
2023-12-15 17:56:56 +0530

    Added pagination to product listing page

--- src/hooks/reactQuery/useProductsApi.js
+++ src/hooks/reactQuery/useProductsApi.js
@@ -13,4 +13,5 @@ export const useFetchProducts = params =>
   useQuery({
     queryKey: [QUERY_KEYS.PRODUCTS, params],
     queryFn: () => productsApi.fetch(params),
+    keepPreviousData: true,
   });
