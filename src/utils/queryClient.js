97d434a3d6b3d85dd83c17e1254b038fc2a4c8e6
jaimysimon <jaimy.simon@bigbinary.com>
2023-12-15 15:14:00 +0530

    Initialized query client instance

---
+++ src/utils/queryClient.js
@@ -0,0 +1,14 @@
+import { QueryClient, QueryCache } from "react-query";
+
+const queryClient = new QueryClient({
+  queryCache: new QueryCache(),
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+      useErrorBoundary: true,
+      staleTime: 3_600_000,
+    },
+  },
+});
+
+export default queryClient;
