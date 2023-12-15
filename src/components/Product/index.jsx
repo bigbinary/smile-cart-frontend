8e8f1f4d98eae001926a77e28366d39d1a694829
Jaimy Simon <jaimy.simon@bigbinary.com>
2023-12-15 16:58:06 +0530

    Created React query custom hook to fetch product and product list (#90)

--- src/components/Product/index.jsx
+++ src/components/Product/index.jsx
@@ -1,12 +1,10 @@
-import { useState, useEffect } from "react";
-
-import productsApi from "apis/products";
 import AddToCart from "components/AddToCart";
 import { Header, PageLoader, PageNotFound } from "components/commons";
+import { useShowProduct } from "hooks/reactQuery/useProductsApi";
 import useSelectedQuantity from "hooks/useSelectedQuantity";
 import i18n from "i18next";
 import { Typography, Button } from "neetoui";
-import { append, isNotNil } from "ramda";
+import { isNotNil } from "ramda";
 import { useTranslation } from "react-i18next";
 import { useParams } from "react-router-dom";
 import routes from "routes";
@@ -15,32 +13,12 @@ import withTitle from "utils/withTitle";
 import Carousel from "./Carousel";
 
 const Product = () => {
-  const [isLoading, setIsLoading] = useState(true);
-  const [isError, setIsError] = useState(false);
-  const [product, setProduct] = useState({});
-
   const { t } = useTranslation();
 
   const { slug } = useParams();
 
+  const { data: product = {}, isLoading, isError } = useShowProduct(slug);
   const { selectedQuantity, setSelectedQuantity } = useSelectedQuantity(slug);
-
-  const fetchProduct = async () => {
-    try {
-      const response = await productsApi.show(slug);
-      setProduct(response);
-    } catch (error) {
-      setIsError(true);
-      // eslint-disable-next-line no-console
-      console.log(t("error.genericError", { error }));
-    } finally {
-      setIsLoading(false);
-    }
-  };
-
-  useEffect(() => {
-    fetchProduct();
-  }, []);
 
   const {
     name,
@@ -68,7 +46,7 @@ const Product = () => {
         <div className="w-2/5">
           <div className="flex justify-center gap-16">
             {isNotNil(imageUrls) ? (
-              <Carousel imageUrls={append(imageUrl, imageUrls)} title={name} />
+              <Carousel />
             ) : (
               <img alt={name} className="w-48" src={imageUrl} />
             )}
