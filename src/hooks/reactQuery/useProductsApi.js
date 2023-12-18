5964b572fa93d76bd5bbfa76f5f4fb1cf957670f
jaimysimon <jaimy.simon@bigbinary.com>
2023-12-18 11:48:16 +0530

    Created React query custom hook to implement parallel queries

--- src/hooks/reactQuery/useProductsApi.js
+++ src/hooks/reactQuery/useProductsApi.js
@@ -1,7 +1,39 @@
 import { QUERY_KEYS } from "constants/query";
 
 import productsApi from "apis/products";
-import { useQuery } from "react-query";
+import { existsBy } from "neetocist";
+import { Toastr } from "neetoui";
+import { prop } from "ramda";
+import { useTranslation } from "react-i18next";
+import { useQuery, useQueries } from "react-query";
+import useCartItemsStore from "stores/useCartItemsStore";
+
+export const useFetchCartProducts = slugs => {
+  const { t } = useTranslation();
+  const { cartItems, setSelectedQuantity } = useCartItemsStore();
+
+  const responses = useQueries(
+    slugs.map(slug => ({
+      queryKey: [QUERY_KEYS.PRODUCTS, slug],
+      queryFn: () => productsApi.show(slug),
+
+      onSuccess: ({ availableQuantity, name }) => {
+        if (availableQuantity >= cartItems[slug]) return;
+        setSelectedQuantity(slug, availableQuantity);
+        if (availableQuantity === 0) {
+          Toastr.error(t("error.removedFromCart", { name }), {
+            autoClose: 2000,
+          });
+        }
+      },
+    }))
+  );
+
+  const isLoading = existsBy({ isLoading: true }, responses);
+  const data = responses.map(prop("data")).filter(Boolean);
+
+  return { isLoading, data };
+};
 
 export const useShowProduct = slug =>
   useQuery({
