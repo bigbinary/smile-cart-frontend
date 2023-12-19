19ebf5d00b413c1cfe2bdf00b901694aa9fc42ff
jaimysimon <jaimy.simon@bigbinary.com>
2023-12-19 14:27:43 +0530

    Utilized babel-preset-neeto to access zustand store properties

--- src/components/Cart/ProductCard.jsx
+++ src/components/Cart/ProductCard.jsx
@@ -3,7 +3,6 @@ import { useState } from "react";
 import ProductQuantity from "components/commons/ProductQuantity";
 import { Delete } from "neetoicons";
 import { Alert, Typography } from "neetoui";
-import { prop } from "ramda";
 import useCartItemsStore from "stores/useCartItemsStore";
 
 const ProductCard = ({
@@ -16,7 +15,7 @@ const ProductCard = ({
 }) => {
   const [shouldShowDeleteAlert, setShouldShowDeleteAlert] = useState(false);
 
-  const removeCartItem = useCartItemsStore(prop("removeCartItem"));
+  const removeCartItem = useCartItemsStore.pickFrom();
 
   return (
     <div className="neeto-ui-rounded neeto-ui-border-black border p-2">
