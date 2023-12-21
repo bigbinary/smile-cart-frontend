b1b5eb899dc2e72ec228a6cd8526ff71d7fb30ca
jaimysimon <jaimy.simon@bigbinary.com>
2023-12-21 16:36:19 +0530

    Created HOC to add page title

--- src/components/Cart/index.jsx
+++ src/components/Cart/index.jsx
@@ -3,10 +3,12 @@ import { useState, useEffect } from "react";
 import productsApi from "apis/products";
 import { PageLoader, Header } from "components/commons";
 import { cartTotalOf } from "components/utils";
+import i18n from "i18next";
 import { NoData, Toastr } from "neetoui";
 import { isEmpty, keys } from "ramda";
 import { useTranslation } from "react-i18next";
 import useCartItemsStore from "stores/useCartItemsStore";
+import withTitle from "utils/withTitle";
 
 import PriceCard from "./PriceCard";
 import ProductCard from "./ProductCard";
@@ -86,4 +88,4 @@ const Cart = () => {
   );
 };
 
-export default Cart;
+export default withTitle(Cart, i18n.t("cart.title"));
