c5a056384e8de7984545af00fcca34fe2e7f3439
jaimysimon <jaimy.simon@bigbinary.com>
2023-12-21 16:38:08 +0530

    Created React query custom hook to fetch product and product list

--- src/components/ProductList/index.jsx
+++ src/components/ProductList/index.jsx
@@ -1,7 +1,7 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 
-import productsApi from "apis/products";
 import { Header, PageLoader } from "components/commons";
+import { useFetchProducts } from "hooks/reactQuery/useProductsApi";
 import useDebounce from "hooks/useDebounce";
 import { Search } from "neetoicons";
 import { Input, NoData } from "neetoui";
@@ -12,28 +12,15 @@ import withTitle from "utils/withTitle";
 import ProductListItem from "./ProductListItem";
 
 const ProductList = () => {
-  const [isLoading, setIsLoading] = useState(true);
-  const [products, setProducts] = useState([]);
   const [searchKey, setSearchKey] = useState("");
 
   const { t } = useTranslation();
 
   const debouncedSearchKey = useDebounce(searchKey);
 
-  const fetchProducts = async () => {
-    try {
-      const data = await productsApi.fetch({ searchTerm: debouncedSearchKey });
-      setProducts(data.products);
-    } catch (error) {
-      console.log(t("error.genericError", { error }));
-    } finally {
-      setIsLoading(false);
-    }
-  };
-
-  useEffect(() => {
-    fetchProducts();
-  }, [debouncedSearchKey]);
+  const { data: { products = [] } = {}, isLoading } = useFetchProducts({
+    searchTerm: debouncedSearchKey,
+  });
 
   if (isLoading) {
     return <PageLoader />;
---
+++ src/hooks/reactQuery/useProductsApi.js
@@ -0,0 +1,16 @@
+import { QUERY_KEYS } from "constants/query";
+
+import productsApi from "apis/products";
+import { useQuery } from "react-query";
+
+export const useShowProduct = slug =>
+  useQuery({
+    queryKey: [QUERY_KEYS.PRODUCTS, slug],
+    queryFn: () => productsApi.show(slug),
+  });
+
+export const useFetchProducts = params =>
+  useQuery({
+    queryKey: [QUERY_KEYS.PRODUCTS, params],
+    queryFn: () => productsApi.fetch(params),
+  });
