6f424f62eaa732925826d65c15db600ea0511b22
jaimysimon <jaimy.simon@bigbinary.com>
2023-12-21 16:43:02 +0530

    Enhanced app performance using memoization

--- src/components/Cart/ProductCard.jsx
+++ src/components/Cart/ProductCard.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, memo } from "react";
 
 import ProductQuantity from "components/commons/ProductQuantity";
 import { Delete } from "neetoicons";
@@ -54,4 +54,4 @@ const ProductCard = ({ slug, imageUrl, offerPrice, mrp, name }) => {
   );
 };
 
-export default ProductCard;
+export default memo(ProductCard);
