8f7fea2ff45549008f6d99e1e4834d5e60156739
jaimysimon <jaimy.simon@bigbinary.com>
2023-12-21 16:41:30 +0530

    Implemented yup validation for checkout form

--- src/components/Checkout/constants.js
+++ src/components/Checkout/constants.js
@@ -1,3 +1,34 @@
+import { t } from "i18next";
+import * as yup from "yup";
+
+export const CHECKOUT_FORM_VALIDATION_SCHEMA = yup.object().shape({
+  email: yup
+    .string()
+    .email(t("validations.emailInvalid"))
+    .required(t("validations.emailRequired")),
+  country: yup
+    .object()
+    .shape({
+      name: yup.string().required(),
+      code: yup.string().required(),
+    })
+    .nullable(),
+  firstName: yup.string().required(t("validations.firstNameRequired")),
+  lastName: yup.string().required(t("validations.lastNameRequired")),
+  address: yup.string().required(t("validations.addressRequired")),
+  apartment: yup.string().required(t("validations.apartmentRequired")),
+  city: yup.string().required(t("validations.cityRequired")),
+  state: yup
+    .object()
+    .shape({
+      name: yup.string().required(),
+      code: yup.string().required(),
+    })
+    .nullable()
+    .required(t("validations.stateRequired")),
+  zipCode: yup.number().required(t("validations.zipCodeRequired")),
+});
+
 export const CHECKOUT_FORM_INITIAL_VALUES = {
   email: "",
   country: { code: "US", name: "United States" },
